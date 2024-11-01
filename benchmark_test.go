87188be4314340d22a676848a44920e43e4e22cd
gram <git@orsinium.dev>
2024-11-01 08:44:57 +0100

    add object

--- benchmark_test.go
+++ benchmark_test.go
@@ -42,3 +42,33 @@ func BenchmarkInt_ToWriter_Stdlib(b *testing.B) {
 		_ = json.NewEncoder(&b).Encode(v)
 	}
 }
+
+func BenchmarkObject_ToWriter_Jsony(b *testing.B) {
+	name := box[string]("john")
+	age := box[int](42)
+	for i := 0; i < b.N; i++ {
+		b := bytes.Buffer{}
+		obj := jsony.Object(
+			jsony.Field("name", jsony.String(name)),
+			jsony.Field("age", jsony.Int(age)),
+		)
+		_ = jsony.Write(&b, obj)
+	}
+}
+
+func BenchmarkObject_ToWriter_Stdlib(b *testing.B) {
+	name := box[string]("john")
+	age := box[int](42)
+	for i := 0; i < b.N; i++ {
+		b := bytes.Buffer{}
+		type User struct {
+			Name string `json:"name"`
+			Age  int    `json:"age"`
+		}
+		v := User{
+			Name: name,
+			Age:  age,
+		}
+		_ = json.NewEncoder(&b).Encode(v)
+	}
+}
---
+++ object.go
@@ -0,0 +1,33 @@
+package jsony
+
+type tField struct {
+	k String
+	v Encoder
+}
+
+type tObject []tField
+
+func Field(k String, v Encoder) tField {
+	return tField{k, v}
+}
+
+func Object(fs ...tField) Encoder {
+	return tObject(fs)
+}
+
+func (v tObject) EncodeJSON(w *SafeWriter) {
+	next := byte('{')
+	for _, f := range v {
+		w.Write([]byte{next, '"'})
+		f.k.EncodeJSON(w)
+		w.Write([]byte{'"', ':'})
+		f.v.EncodeJSON(w)
+		next = ','
+	}
+	if next == '{' {
+		w.Write([]byte{'{', '}'})
+	} else {
+		w.Write([]byte{'}'})
+	}
+
+}
