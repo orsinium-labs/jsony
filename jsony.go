ba5a116f658e18f69afadcab7100384ef805ee89
gram <git@orsinium.dev>
2024-11-01 08:59:20 +0100

    bufferize output

--- jsony.go
+++ jsony.go
@@ -9,28 +9,45 @@ type Encoder interface {
 	EncodeJSON(*SafeWriter)
 }
 
-// SafeWriter wraps io.Writer and instead of returning an error stores it.
+// SafeWriter wraps io.Writer.
+//
+// Buffers writes and stores write errors internally.
 type SafeWriter struct {
-	W   io.Writer
+	w   io.Writer
+	buf []byte
 	Err error
 }
 
 func (w *SafeWriter) Write(b []byte) {
-	_, err := w.W.Write(b)
+	w.buf = append(w.buf, b...)
+	if len(w.buf) > 1024 {
+		w.Flush()
+	}
+}
+
+func (w *SafeWriter) Flush() {
+	_, err := w.w.Write(w.buf)
 	if err != nil && w.Err == nil {
 		w.Err = err
 	}
+	w.buf = w.buf[:0]
 }
 
 func EncodeString(e Encoder) string {
 	iw := bytes.Buffer{}
-	sw := SafeWriter{W: &iw}
+	sw := SafeWriter{
+		w:   &iw,
+		buf: nil,
+	}
 	e.EncodeJSON(&sw)
 	return iw.String()
 }
 
 func Write(w io.Writer, e Encoder) error {
-	sw := SafeWriter{W: w}
+	sw := SafeWriter{
+		w:   w,
+		buf: nil,
+	}
 	e.EncodeJSON(&sw)
 	return sw.Err
 }
