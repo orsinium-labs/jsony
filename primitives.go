dc657b3da9803e12271ceb9cc6aecf1885b8845b
gram <git@orsinium.dev>
2024-11-01 08:02:44 +0100

    uint

--- primitives.go
+++ primitives.go
@@ -3,15 +3,23 @@ package jsony
 import "strconv"
 
 type (
-	Bool  bool
-	Int   int
-	Int32 int32
+	Bool   bool
+	Int    int
+	Int32  int32
+	Int64  int64
+	UInt   uint
+	UInt32 uint32
+	UInt64 uint64
 )
 
 var (
 	_ Encoder = Bool(true)
 	_ Encoder = Int(0)
 	_ Encoder = Int32(0)
+	_ Encoder = Int64(0)
+	_ Encoder = UInt(0)
+	_ Encoder = UInt32(0)
+	_ Encoder = UInt64(0)
 )
 
 func (v Bool) EncodeJSON(w *SafeWriter) {
@@ -29,5 +37,25 @@ func (v Int) EncodeJSON(w *SafeWriter) {
 
 func (v Int32) EncodeJSON(w *SafeWriter) {
 	b := strconv.AppendInt(nil, int64(v), 10)
+	w.Write(b)
+}
+
+func (v Int64) EncodeJSON(w *SafeWriter) {
+	b := strconv.AppendInt(nil, int64(v), 10)
+	w.Write(b)
+}
+
+func (v UInt) EncodeJSON(w *SafeWriter) {
+	b := strconv.AppendUint(nil, uint64(v), 10)
+	w.Write(b)
+}
+
+func (v UInt32) EncodeJSON(w *SafeWriter) {
+	b := strconv.AppendUint(nil, uint64(v), 10)
+	w.Write(b)
+}
+
+func (v UInt64) EncodeJSON(w *SafeWriter) {
+	b := strconv.AppendUint(nil, uint64(v), 10)
 	w.Write(b)
 }
